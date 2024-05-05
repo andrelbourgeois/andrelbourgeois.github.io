71d88b8578816fb0dfb7dbd7a19f4ef72f9e1ffc
André Bourgeois <andrelbourgeois@gmail.com>
2024-05-05 22:14:31 +0100

    fixed name error

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,7 +15,7 @@ export function MainNav({ items }: MainNavProps) {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         <Icons.logo className="h-8 w-8" />
-        <span className="inline-block font-bold pl-2">{siteConfig.name}</span>
+        {/*<span className="inline-block font-bold pl-2">{siteConfig.name}</span>*/}
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
